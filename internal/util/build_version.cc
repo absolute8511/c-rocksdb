f29196b02a622f8b00221cdff7517ceb91116a18
Vincent Lee <cool8511@gmail.com>
2017-01-07 21:45:06 +0800

    update build version

--- internal/util/build_version.cc
+++ internal/util/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:f2efe57d4e541ddaa7dd324da1b1444dde5188e7";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:5ad386bf5c3111553107b6f93c3f1c9308071f92";
 const char* rocksdb_build_git_date = "rocksdb_build_git_date:2017-01-07";
 const char* rocksdb_build_compile_date = __DATE__;
